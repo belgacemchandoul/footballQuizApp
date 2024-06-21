ba9998e6ba82f5c4c30cabcbe455956cbec24c86
belgalc <bchandoul6@gmail.com>
2024-06-22 00:12:43 +0100

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex flex-col items-center gap-7 h-screen justify-center">
-      <div className="uppercase text-9xl">hala madrid</div>
+      <div className="uppercase text-9xl">hala madridaaaaa</div>
       <img src="../src/assets/Real Madrid CF.png" className="w-28" />
     </div>
   );
