4ecbc5e10d2d57c96862a9dd95c6b160dfec4a38
belgalc <bchandoul6@gmail.com>
2024-07-02 18:10:42 +0100

    logic end

--- src/pages/WhoHasMoreGoals.tsx
+++ src/pages/WhoHasMoreGoals.tsx
@@ -1,5 +1,95 @@
+import { useState } from "react";
+import Layout from "../components/Layout";
+import usePlayersData from "../hooks/usePlayersData";
+import Player from "../types/players";
+import randomizeId from "../utils/randomizeId";
+
 const WhoHasMoreGoals = () => {
-  return <div>Who has more goals</div>;
+  const [basePlayer, setBasePlayer] = useState<Player | null>(null);
+  const [nextPlayer, setNextPlayer] = useState<Player | null>(null);
+  const { players, loading, error } = usePlayersData();
+
+  const handleGameStart = () => {
+    let validPlayerFound = false;
+    while (!validPlayerFound) {
+      const basePlayerRandomId = randomizeId(players);
+      const nextPlayerRandomId = randomizeId(players);
+      const basePlayer = players[basePlayerRandomId];
+      const nextPlayer = players[nextPlayerRandomId];
+
+      if (
+        basePlayer.goals !== null &&
+        nextPlayer.goals !== null &&
+        nextPlayer.goals !== basePlayer.goals
+      ) {
+        setBasePlayer(basePlayer);
+        setNextPlayer(nextPlayer);
+        validPlayerFound = true;
+      }
+    }
+  };
+  const nextPlayerGeneration = (currPlayer: Player) => {
+    let validPlayerFound = false;
+    while (!validPlayerFound) {
+      const nextPlayerRandomId = randomizeId(players);
+      const genPlayer = players[nextPlayerRandomId];
+
+      if (
+        currPlayer.goals !== null &&
+        genPlayer.goals !== null &&
+        genPlayer.goals !== currPlayer.goals
+      ) {
+        setNextPlayer(genPlayer);
+        validPlayerFound = true;
+      }
+    }
+  };
+  const handleGoalsCheck = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { value } = e.currentTarget;
+    if (
+      basePlayer === null ||
+      basePlayer.goals === null ||
+      nextPlayer === null ||
+      nextPlayer.goals === null
+    )
+      return;
+    if (basePlayer.goals < nextPlayer.goals && value === "more") {
+      setBasePlayer(nextPlayer);
+      nextPlayerGeneration(nextPlayer);
+    } else if (basePlayer.goals > nextPlayer.goals && value === "less") {
+      setBasePlayer(nextPlayer);
+      nextPlayerGeneration(nextPlayer);
+    } else {
+      console.log("game over");
+    }
+  };
+
+  if (loading) {
+    return <div>Loading..</div>;
+  }
+  if (error) {
+    console.error(error);
+  }
+  return (
+    <Layout>
+      <div>Who Has More Goals ?</div>
+      <section>
+        <button onClick={handleGameStart}>start</button>
+        <div>
+          {basePlayer?.name} {basePlayer?.goals}
+          <div>
+            {nextPlayer?.name} {nextPlayer?.goals}
+            <button onClick={(e) => handleGoalsCheck(e)} value="more">
+              more
+            </button>
+            <button onClick={(e) => handleGoalsCheck(e)} value="less">
+              less
+            </button>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default WhoHasMoreGoals;
--- src/pages/WhoIsMissing.tsx
+++ src/pages/WhoIsMissing.tsx
@@ -1,5 +1,60 @@
+import { useRef, useState } from "react";
+import Layout from "../components/Layout";
+import useSquadsData from "../hooks/useSquadsData";
+import Squad from "../types/squads";
+import randomizeId from "../utils/randomizeId";
+import normalizeString from "../utils/normalizeString";
+
 const WhoIsMissing = () => {
-  return <div>Who is missing</div>;
+  const { squads, loading, error } = useSquadsData();
+  const [selectedSquad, setSelectedSquad] = useState<Squad | null>(null);
+  const [gamePhase, setGamePhase] = useState<string>("welcome");
+  const [score, setScore] = useState<number>(0);
+  const [input, setInput] = useState<string>("");
+  const maxNumRef = useRef(0);
+
+  const handleGameStart = () => {
+    setGamePhase("start");
+    if (maxNumRef.current >= 5) {
+      setGamePhase("over");
+      return;
+    }
+    const randomId = randomizeId(squads);
+    setSelectedSquad(squads[randomId]);
+    maxNumRef.current += 1;
+  };
+  const handleGuess = () => {
+    if (selectedSquad === null) return;
+    const normalizedGuess = normalizeString(input);
+    const normalizedPlayerName = normalizeString(selectedSquad?.missingPlayer);
+    if (normalizedGuess === normalizedPlayerName) {
+      console.log("saha");
+      setScore((prevScore) => (prevScore += 1));
+      setInput("");
+      handleGameStart();
+    } else {
+      console.log("aasba");
+      setInput("");
+      handleGameStart();
+    }
+  };
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  if (error) {
+    console.error(error);
+  }
+  return (
+    <Layout>
+      <div>Who Is Missing?</div>
+      <section>
+        <button onClick={handleGameStart}>start</button>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={handleGuess}>Guess</button>
+      </section>
+      {gamePhase === "over" && <div>your score is {score}</div>}
+    </Layout>
+  );
 };
 
 export default WhoIsMissing;
